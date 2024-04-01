c6534c3e9ce369d0d08573f859e877f4a1b00116
aespitoj <aespitoj@gmail.com>
2024-04-01 17:12:51 +0500

    Add macro to index.html

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 
+from wtform_fields import *
+
+#Configure app
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
@@ -7,8 +10,11 @@ app.secret_key = 'replace later'
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
+	reg_form = RegistrationForm()
+	if reg_form.validate_on_submit():
+		return "Great Success"
 	#return 'I am alive!'
-	return render_template("index.html")
+	return render_template("index.html", form=reg_form)
 	
 	
 if __name__ == "__main__":
